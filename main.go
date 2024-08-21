05fbc758c3b5fbb967f5f0547a0ad14fa8ccc6fc
zarubin_d_v <denis.zarubin@gmail.com>
2024-08-21 16:08:38 +0300

    fix

--- main.go
+++ main.go
@@ -11,17 +11,19 @@ type Acc struct {
 }
 
 func main() {
+	for {
+		saturday, sunday := GetNextWeekendDates()
+		sunday = sunday.Add(time.Hour * 24)
+		saturday = saturday.Add(-time.Hour * 24)
+		text := "Здравствуйте. Если Вы хотите сообщить о нежелательной реакции на лекарственный препарат, свяжитесь с уполномоченным лицом по фармаконадзору по телефону +375445867515 или по электронной почте head_pv@ft.by"
+		fromDate := saturday.Format("20060102") + "134000Z"
+		toDate := sunday.Format("20060102") + "045900Z"
 
-	saturday, sunday := GetNextWeekendDates()
-	sunday = sunday.Add(time.Hour * 24)
-	saturday = saturday.Add(-time.Hour * 24)
-	text := "Здравствуйте. Если Вы хотите сообщить о нежелательной реакции на лекарственный препарат, свяжитесь с уполномоченным лицом по фармаконадзору по телефону +375445867515 или по электронной почте head_pv@ft.by"
-	fromDate := saturday.Format("20060102") + "133000Z"
-	toDate := sunday.Format("20060102") + "045900Z"
-
-	for _, acc := range getAccs() {
-		log.Print(acc.Login)
-		sendToZimbra(acc, text, fromDate, toDate)
+		for _, acc := range getAccs() {
+			log.Print(acc.Login)
+			sendToZimbra(acc, text, fromDate, toDate)
+		}
+		time.Sleep(24 * 7 * time.Hour)
 	}
 
 }
